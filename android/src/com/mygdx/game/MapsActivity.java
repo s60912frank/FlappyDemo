62c64c949d57af0dda38d284676e16bb111d8981
unknown <>
2016-03-15 11:54:09 +0800

    Add something on map.

--- android/src/com/mygdx/game/MapsActivity.java
+++ android/src/com/mygdx/game/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,6 +9,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,7 +44,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("危險區域!").icon(BitmapDescriptorFactory.fromAsset("danger.png")));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addCircle(new CircleOptions().center(sydney).radius(10000).strokeColor(Color.argb(255,255,0,0)).fillColor(Color.argb(100,255,0,0)).strokeWidth(3.5f));
     }
 }
